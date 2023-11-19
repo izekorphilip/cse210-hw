ea8d93d4857b92490fcc235c7d417c2c10b5968a
izekorphilip <izekorphilip5@gmail.com>
2023-11-19 18:21:02 +0100

    Abstraction Learning Activity completed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        public class Person
+        {
+            
+            public string _givenName = "Jonh";
+            public string _familyName = "peter";
+
+            public Person ()
+            {
+
+            }
+            public void ShowEasternName()
+            {
+                Console.WriteLine($"{_givenName} {_familyName}");
+            }
+            Person.ShowEasternName()
+        }
     }
 }
